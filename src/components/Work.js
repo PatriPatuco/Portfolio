db68986eac27b7086925522d6bfcbf2850eae819
Patuco <patricia.beco.dg@gmail.com>
2023-05-30 10:51:21 +0200

    Add links and info about the projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,18 +1,41 @@
 import projects from "./services/ProjectsData.js";
+import React, { useState } from "react";
 
 function Work() {
-const renderProjects = () => {
-  return projects.map((project) =>
+  const [showInfo, setShowInfo] = useState(null);
 
-  <article key={project.id} className="work__project">
-    <h2 className="work__project--title">{project.name}</h2>
-    <p className="work__project--languages">{project.languages}</p>
-    <p className="work__project--info">{project.info}</p>
-    </article>
-  )
-}
+  const handleMouseEnter = (id) => {
+    setShowInfo(id);
+  }
 
+  const handleMouseLeave = () => {
+    setShowInfo(null);
+  }
 
+  // Print projects
+  const renderProjects = () => {
+    return projects.map((project) => (
+      <article key={project.id} className="work__project">
+        <a className="work__project--link" href={project.link}>
+          <h2
+            className="work__project--title"
+            onMouseEnter={() => handleMouseEnter(project.id)}
+            onMouseLeave={handleMouseLeave}
+          >
+            {project.name}
+          </h2>
+        </a>
+        <p className="work__project--languages">{project.languages}</p>
+        <p
+          className={
+            showInfo === project.id ? "show-info" : "work__project--info"
+          }
+        >
+          {project.info}
+        </p>
+      </article>
+    ));
+  };
 
   return (
     <section className="work" id="work">
@@ -20,7 +43,7 @@ const renderProjects = () => {
         <div className="menu-line"></div>
         <p>Work</p>
       </aside>
-        {renderProjects()}
+      {renderProjects()}
     </section>
   );
 }
--- src/components/services/ProjectsData.js
+++ src/components/services/ProjectsData.js
@@ -3,26 +3,30 @@ const projects = [
     id: 1,
     name: "Harry Potter wiki",
     languages: "React - SCSS - HTML - HP Api",
-    info: "Wiki project dedicated to Harry Potter Saga. You can do the followingthings on the site: Filter by gender, Hogwarts house, and character name. Check the entire list of characters. Visit the details page and discover more info about each character.",
+    link: "https://patripatuco.github.io/harry-potter-wiki/",
+    info: "Wiki project dedicated to Harry Potter Saga. You can do the following things on the site: Filter by gender, Hogwarts house, and character name / Check the entire list of characters / Visit the details page and discover more info about each character.",
   },
   {
     id: 2,
     name: "Proyectos Canelo",
+    link: "https://proyecto-canelo.onrender.com/",
     languages: "React - SCSS - HTML - MySQL",
-    info: "",
+    info: "Create your own project card and share it everywhere. If you want to test it, you just need to fill out the form and click 'Create card' and it will generate your personal URL link with all the information. You can also reset the form or check it on the main page with the rest of the projects in the database.",
   },
   {
     id: 3,
     name: "Guess the number",
+    link: "https://patripatuco.github.io/guess-the-number/",
     languages: "JavaScript - SCSS - HTML",
-    info: "",
+    info: "A small game page where you need to guess the number randomly picked by the computer. To try it, write your option in the input and press the button down below. If the answer is wrong, you will receive a tip for the next try and the number of attempts will increase until you find the solution. If you try to write a letter or any character beyond a number, it will throw an error message",
   },
   {
     id: 4,
     name: "Cocktail Hunt",
+    link: "https://patripatuco.github.io/cocktail-hunt/",
     languages: "JavaScript - SCSS - HTML",
-    info: "",
-  }
+    info: "A cocktail search project to look at all types of cocktails showing their name and image. You can add your personal choices in the favorites column by clicking on them. You can remove the entire list by clicking the 'Delete List' button or a single one by clicking the X button.",
+  },
 ];
 
 export default projects;

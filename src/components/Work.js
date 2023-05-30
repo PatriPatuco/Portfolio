144036956ab059c1562815ab85bf39f6c8c2dd70
Patuco <patricia.beco.dg@gmail.com>
2023-05-30 10:09:37 +0200

    Add map function to print projects + create projectsData file

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,32 +1,26 @@
+import projects from "./services/ProjectsData.js";
+
 function Work() {
+const renderProjects = () => {
+  return projects.map((project) =>
+
+  <article key={project.id} className="work__project">
+    <h2 className="work__project--title">{project.name}</h2>
+    <p className="work__project--languages">{project.languages}</p>
+    <p className="work__project--info">{project.info}</p>
+    </article>
+  )
+}
+
+
+
   return (
     <section className="work" id="work">
       <aside className="work__aside menu-left">
         <div className="menu-line"></div>
         <p>Work</p>
       </aside>
-      <article className="work__project">
-        <h2 className="work__project--title">Harry Potter wiki</h2>
-        <p className="work__project--languages">React - SCSS - HTML - HP Api</p>
-        <p className="work__project--description">
-          Wiki project dedicated to Harry Potter Saga. You can do the following
-          things on the site: Filter by gender, Hogwarts house, and character
-          name. Check the entire list of characters. Visit the details page and
-          discover more info about each character.
-        </p>
-      </article>
-      <article className="work__project">
-        <h2 className="work__project--title">Proyectos Canelo</h2>
-        <p className="work__project--languages">React - SCSS - HTML - MySQL</p>
-      </article>
-      <article className="work__project">
-        <h2 className="work__project--title">Guess the number</h2>
-        <p className="work__project--languages">JavaScritp - SCSS - HTML</p>
-      </article>
-      <article className="work__project">
-        <h2 className="work__project--title">Cocktail Hunt</h2>
-        <p className="work__project--languages">JavaScript - SCSS - HTML</p>
-      </article>
+        {renderProjects()}
     </section>
   );
 }
---
+++ src/components/services/ProjectsData.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    id: 1,
+    name: "Harry Potter wiki",
+    languages: "React - SCSS - HTML - HP Api",
+    info: "Wiki project dedicated to Harry Potter Saga. You can do the followingthings on the site: Filter by gender, Hogwarts house, and character name. Check the entire list of characters. Visit the details page and discover more info about each character.",
+  },
+  {
+    id: 2,
+    name: "Proyectos Canelo",
+    languages: "React - SCSS - HTML - MySQL",
+    info: "",
+  },
+  {
+    id: 3,
+    name: "Guess the number",
+    languages: "JavaScript - SCSS - HTML",
+    info: "",
+  },
+  {
+    id: 4,
+    name: "Cocktail Hunt",
+    languages: "JavaScript - SCSS - HTML",
+    info: "",
+  }
+];
+
+export default projects;

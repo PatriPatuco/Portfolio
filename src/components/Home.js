e1f176865c105ffb2680090bab36933ac6d4dc1f
Patuco <patricia.beco.dg@gmail.com>
2023-06-01 14:42:04 +0200

    title animation first steps

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,44 @@
+import React, { useEffect } from "react";
 import Typewriter from "typewriter-effect";
 
 const Home = () => {
+  useEffect(() => {
+    const text = document.querySelector(".text");
+    const letters = Array.from(text.querySelectorAll("li"));
+
+    setTimeout(() => {
+      text.classList.remove("hidden");
+      letters[0].classList.add("typing-animation");
+    }, 2000); // Ajusta el tiempo de espera según tus preferencias
+  }, []);
+
   return (
     <section className="home">
-      <h3 className="home__subtitle hello">Hello, world! my name is</h3>
-      <h1 className="home__title">PATRICIA BECERRA</h1>
+      <h3 className="home__subtitle">Hello, world! my name is</h3>
+      <ul className="home__title text hidden">
+        <li className="home__span">&lt;</li>
+        <li>P</li>
+        <li className="ghost">A</li>
+        <li className="ghost">T</li>
+        <li className="ghost">R</li>
+        <li className="ghost">I</li>
+        <li className="ghost">C</li>
+        <li className="ghost">I</li>
+        <li className="ghost">A</li>
+        <li className="spaced">B</li>
+        <li className="ghost">E</li>
+        <li className="ghost">C</li>
+        <li className="ghost">E</li>
+        <li className="ghost">R</li>
+        <li className="ghost">R</li>
+        <li className="ghost">A</li>
+        <li className="home__span spaced">/&gt;</li>
+      </ul>
       <h3 className="home__subtitle">
         <Typewriter
           onInit={(typewriter) => {
             typewriter
+              .pauseFor(2000)
               .changeDelay(90)
               .typeString("Frontend Developer")
               .pauseFor(1000)
@@ -18,7 +48,6 @@ const Home = () => {
               .deleteAll()
               .typeString("Frontend Developer & Graphic Designer")
               .start();
-              
           }}
         />
       </h3>

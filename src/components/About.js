d81974a381c9bd93a111faab4bd32b412f644bd0
Patuco <patricia.beco.dg@gmail.com>
2023-06-06 12:23:39 +0200

    Add desktop queries

--- src/components/About.js
+++ src/components/About.js
@@ -13,30 +13,28 @@ function About() {
         <p>About</p>
         <div className="menu-line"></div>
       </aside>
+
+      <img src={patri} alt="Patricia Becerra portrait" className="about__img" />
       <article className="about__resume">
-        <img
-          src={patri}
-          alt="Patricia Becerra portrait"
-          className="about__resume--img"
-        />
         <p className="regular-text">
           Hey! I'm Patricia, passionate about graphic design and web
-          development.</p>
-          <p className="regular-text">
-            I have been in the design world for over 5 years, but thanks to my
-            previous job, I started to be very interested in web development.
-            That is the reason why I dig more into the technology world.
-          </p>
-          <p className="regular-text">
-            I consider myself a resourceful and committed person who loves team
-            work. In addition, my experience as a designer has taught me how to
-            work under pressure and deliver the best results in difficult
-            situations
-          </p>
-          <p className="regular-text">
-            Outside of work, I enjoy my free time playing video games, watching
-            some series, and spending time with my friends.
-          </p>
+          development.
+        </p>
+        <p className="regular-text">
+          I have been in the design world for over 5 years, but thanks to my
+          previous job, I started to be very interested in web development. That
+          is the reason why I dig more into the technology world.
+        </p>
+        <p className="regular-text">
+          I consider myself a resourceful and committed person who loves team
+          work. In addition, my experience as a designer has taught me how to
+          work under pressure and deliver the best results in difficult
+          situations
+        </p>
+        <p className="regular-text">
+          Outside of work, I enjoy my free time playing video games, watching
+          some series, and spending time with my friends.
+        </p>
       </article>
 
       <article className="about__info">
@@ -55,12 +53,12 @@ function About() {
           <img
             src={Gestorex}
             alt="Gestorex logotype"
-            className="about__companies--logo"
+            className="about__companies--logo gestorex"
           ></img>
           <img
             src={MonoGrafica}
             alt="MonoGrafica logotype"
-            className="about__companies--logo"
+            className="about__companies--logo monografica"
           ></img>
         </div>
 
@@ -74,12 +72,16 @@ function About() {
           <img
             src={EASDM}
             alt="EASDM logotype"
-            className="about__companies--logo"
+            className="about__companies--logo easdm"
           ></img>
         </div>
       </article>
 
-      <a className="about__button" href="./Patricia-Becerra-Corbacho-CV.pdf" download>
+      <a
+        className="about__button"
+        href="./Patricia-Becerra-Corbacho-CV.pdf"
+        download
+      >
         <i className="fa-solid fa-arrow-down"></i>
         <p>Download CV</p>
       </a>
